88939f7257137ad2b37fb74fdad3eee266bd9d02
Eric <coralvanda@gmail.com>
2016-06-16 12:38:35 +0200

    Add code from instructor solution to registration

--- engineapp-backend-intro/main.py
+++ engineapp-backend-intro/main.py
@@ -22,6 +22,7 @@ import re
 import hmac
 import Cookie
 import datetime
+import random
 
 from google.appengine.ext import db
 
@@ -99,6 +100,52 @@ def check_secure_val(h):
 	if h == make_secure_val(val):
 		return val
 
+def make_salt(length=5):
+	#TODO: fix the random choice of letters
+	return "".join(random.choice(letters) for x in range(length))
+
+def make_pw_hash(name, pw, salt=None):
+	if not salt:
+		salt = make_salt()
+	h = hashlib.sha256(name + pw + salt).hexdigest()
+	return '%s,%s' % (salt, h)
+
+def valid_pw(name, password, h):
+	salt = h.split(',')[0]
+	return h == make_pw_hash(name, password, salt)
+
+def users_key(group = 'default'):
+	return db.Key.from_path('users', group)
+
+
+class User(db.Model):
+	name = db.StringProperty(required=True)
+	pw_hash = db.StringProperty(required=True)
+	email = db.StringProperty()
+
+	@classmethod
+	def by_id(cls, uid):
+		return User.get_by_id(uid, parent = user_key())
+
+	@classmethod
+	def by_name(cls, name):
+		u = User.all().filter('name =', name).get()
+		return u
+
+	@classmethod
+	def register(cls, name, pw, email = None):
+		pw_hash = make_pw_hash(name, pw)
+		return User(parent=users_key(),
+			name=name,
+			pw_hash=pw_hash,
+			email=email)
+
+	@classmethod
+	def login(cls, name, pw):
+		u = self.by_name(name)
+		if u and valid_pw(name, pw, u.pw_hash):
+			return u
+
 
 class SignupHandler(Handler):
 	"""Allows a user to signup, enforcing rules for username, password
@@ -119,6 +166,10 @@ class SignupHandler(Handler):
 		email_error		= ''
 		valid_form = True
 
+		u = User.by_name(user_name)
+		if u:
+			username_error = "That user already exists."
+			valid_form = False
 		if not valid_username(user_name):
 			username_error = "Please enter a valid user name"
 			valid_form = False
@@ -133,6 +184,8 @@ class SignupHandler(Handler):
 			valid_form = False
 
 		if valid_form:
+			u = User.register(user_name, password, email)
+			u.put()
 			cookie_val = make_secure_val(user_name)
 			self.response.set_cookie('name', cookie_val)
 			self.redirect('/welcome')
